335e666b61fa9b5f6046dc08b4a7216d5c0b3ddd
mnhmilu <mnh_milu@yahoo.com>
2023-09-03 14:28:37 +0600

    base template added

--- crudexample/urls.py
+++ crudexample/urls.py
@@ -19,8 +19,9 @@ from django.urls import path
 from employee import views  
 urlpatterns = [  
     path('admin/', admin.site.urls),  
-    path('emp', views.emp),  
-    path('show',views.show),  
+    path('dashboard/',views.home),
+    path('employee/emp', views.emp),  
+    path('employee/show',views.show),  
     path('edit/<int:id>', views.edit),  
     path('update/<int:id>', views.update),  
     path('delete/<int:id>', views.destroy),  
--- employee/views.py
+++ employee/views.py
@@ -2,32 +2,37 @@ from django.shortcuts import render, redirect
 from employee.forms import EmployeeForm  
 from employee.models import Employee  
 # Create your views here.  
+
+def home(request):
+    return render(request,"dashboard.html");
+
+
 def emp(request):  
     if request.method == "POST":  
         form = EmployeeForm(request.POST)  
         if form.is_valid():  
             try:  
                 form.save()  
-                return redirect('/show')  
+                return redirect('employee/show')  
             except:  
                 pass  
     else:  
         form = EmployeeForm()  
-    return render(request,'index.html',{'form':form})  
+    return render(request,'employee/index.html',{'form':form})  
 def show(request):  
     employees = Employee.objects.all()  
-    return render(request,"show.html",{'employees':employees})  
+    return render(request,"employee/show.html",{'employees':employees})  
 def edit(request, id):  
     employee = Employee.objects.get(id=id)  
-    return render(request,'edit.html', {'employee':employee})  
+    return render(request,'employee/edit.html', {'employee':employee})  
 def update(request, id):  
     employee = Employee.objects.get(id=id)  
     form = EmployeeForm(request.POST, instance = employee)  
     if form.is_valid():  
         form.save()  
-        return redirect("/show")  
-    return render(request, 'edit.html', {'employee': employee})  
+        return redirect("employee/show")  
+    return render(request, 'employee/edit.html', {'employee': employee})  
 def destroy(request, id):  
     employee = Employee.objects.get(id=id)  
     employee.delete()  
-    return redirect("/show")  
+    return redirect("employee/show")  
