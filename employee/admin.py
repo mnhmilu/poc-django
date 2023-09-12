569ccb82d3ebc9a907f390d89950cb0a453457f8
mnhmilu <mnh_milu@yahoo.com>
2023-09-12 12:28:24 +0600

    filter added in admin.py

--- employee/admin.py
+++ employee/admin.py
@@ -4,6 +4,12 @@
 from django.contrib import admin
 from .models import Employee,Project,Event
 
+
+class ProjectAdmin(admin.ModelAdmin):
+    search_fields = ["team_lead", "due_date"]
+    list_filter = ("due_date",)
+
 admin.site.register(Employee)
-admin.site.register(Project)
+admin.site.register(Project,ProjectAdmin)
 admin.site.register(Event)
+
